cf1677dcfe30c04fc92e6ab4f2315bc90dc98e90
Aurensar <markb.uk@gmail.com>
2022-10-15 20:14:21 +0100

    Added debug error logging to fix crash

--- script/logger.lua
+++ script/logger.lua
@@ -8,6 +8,7 @@ end
 local function log2(string)
     if __DebugAdapter then
         __DebugAdapter.print("[FI] "..string)
+        game.print("[FI] "..string)
     end
     --game.print("[FI] "..string)
 end
@@ -23,5 +24,6 @@ end
 
 return {
     log = log,
-    log2 = log2
+    log2 = log2,
+    error = error
   }  
